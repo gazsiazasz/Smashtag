14a844be3b2b13d682a7c6b4c058915142486dc5
Gajdos Gábor <gazsiazasz@gmail.com>
2017-09-09 15:35:23 +0200

    Initial Commit

---
+++ Smashtag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smashtag
+//
+//  Created by Gajdos Gábor on 2017. 09. 09..
+//  Copyright © 2017. Gajdos Gábor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
